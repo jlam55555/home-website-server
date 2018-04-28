2477f06e32de1de2e3652dd8b549ae54f1a9a579
Jonathan Lam <jlam55555@gmail.com>
2018-04-27 21:35:58 -0400

    new build with recipes almost completed

---
+++ data/ingredientUnits.js
@@ -0,0 +1,24 @@
+module.exports = [
+  { value: null,          text: '(count)' },
+  // units of volume
+  { value: 'teaspoon',    text: 'tsp.' },
+  { value: 'fluidOunce',  text: 'fl oz.' },
+  { value: 'cup',         text: 'cup' },
+  { value: 'pint',        text: 'pt.' },
+  { value: 'gallon',      text: 'gal.' },
+  { value: 'milliliter',  text: 'mL' },
+  { value: 'liter',       text: 'L' },
+  // mass and weight
+  { value: 'pound',       text: 'lb.' },
+  { value: 'ounce',       text: 'oz.' },
+  { value: 'milligram',   text: 'mg' },
+  { value: 'gram',        text: 'g' },
+  { value: 'kilogram',    text: 'kg' },
+  // length
+  { value: 'millimeter',  text: 'mm' },
+  { value: 'centimeter',  text: 'cm' },
+  { value: 'meter',       text: 'm' },
+  { value: 'inch',        text: 'inch.' },
+  // other (will be hidden in recipe)
+  { value: 'other',       text: '(other)' },
+];
